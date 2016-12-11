44fbdf6aa1470e00f0c38058093581ba2c45e65a
Nitin Prakash <nitin247@outlook.com>
2016-12-11 14:04:53 +0530

    Updated Analytics code

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <html>
 
 <head>
-  <title>NPCI UPI QR CODE GENERATOR - Coming Soon</title>
+  <title>NPCI UPI QR CODE GENERATOR</title>
   <meta name="description" content="NPCI UPI QR CODE GENERATOR is a PHP Script and class to Generate UPI" />
   <meta name="keywords" content="NCPI CODE GENERATOR,UPI CODE GENERATOR,FREE NCPI CODE GENERATOR,FREE UPI CODE GENERATOR" />
   <meta name="copyright" content="Webcurries.com" />
@@ -31,6 +31,17 @@
 
   <!-- jQuery -->
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-88805197-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 
 </head>
 <body>
